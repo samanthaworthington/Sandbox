f9b5bb8e8bec2141f5425839fbbd29f689a11723
Samantha Worthington <samantha.worthington@my.jcu.edu.au>
2017-08-16 13:45:47 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,14 @@
 """Samantha Worthington"""
+
+name = str(input("What is your name? "))
+while len(name)<= 0:
+    print("No name entered")
+    name = str(input("What is your name? "))
+
+print(name[0:len(name):2])
+
+
+
+
+
+
